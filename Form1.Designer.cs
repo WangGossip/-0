d049f9cb5a4333556aa5052c83063986e15222f7
unknown <982728182@qq.com>
2019-12-21 22:03:26 +0800

    资产处兼职第一次提交（旧版本）

---
+++ Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace WindowsForms_Word_To_Excel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.使用帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.导入文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.选择保存路径ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.开始ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.选择文件模板ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.使用帮助ToolStripMenuItem,
+            this.导入文件ToolStripMenuItem,
+            this.选择保存路径ToolStripMenuItem,
+            this.选择文件模板ToolStripMenuItem,
+            this.开始ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(934, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 使用帮助ToolStripMenuItem
+            // 
+            this.使用帮助ToolStripMenuItem.Name = "使用帮助ToolStripMenuItem";
+            this.使用帮助ToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.使用帮助ToolStripMenuItem.Text = "使用帮助";
+            // 
+            // 导入文件ToolStripMenuItem
+            // 
+            this.导入文件ToolStripMenuItem.Name = "导入文件ToolStripMenuItem";
+            this.导入文件ToolStripMenuItem.Size = new System.Drawing.Size(113, 24);
+            this.导入文件ToolStripMenuItem.Text = "选择输入文件";
+            this.导入文件ToolStripMenuItem.Click += new System.EventHandler(this.导入文件ToolStripMenuItem_Click);
+            // 
+            // 选择保存路径ToolStripMenuItem
+            // 
+            this.选择保存路径ToolStripMenuItem.Name = "选择保存路径ToolStripMenuItem";
+            this.选择保存路径ToolStripMenuItem.Size = new System.Drawing.Size(113, 24);
+            this.选择保存路径ToolStripMenuItem.Text = "选择保存路径";
+            this.选择保存路径ToolStripMenuItem.Click += new System.EventHandler(this.选择保存路径ToolStripMenuItem_Click);
+            // 
+            // 开始ToolStripMenuItem
+            // 
+            this.开始ToolStripMenuItem.Name = "开始ToolStripMenuItem";
+            this.开始ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.开始ToolStripMenuItem.Text = "开始";
+            this.开始ToolStripMenuItem.Click += new System.EventHandler(this.开始ToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.Location = new System.Drawing.Point(12, 73);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(264, 335);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(194, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 425);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(493, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(334, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "在这里输入文件名：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 425);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "进度信息：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(172, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "当前选择的输入文件为：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(187, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "当前选择的导出文件夹为：";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(197, 55);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 15);
+            this.label7.TabIndex = 9;
+            // 
+            // 选择文件模板ToolStripMenuItem
+            // 
+            this.选择文件模板ToolStripMenuItem.Name = "选择文件模板ToolStripMenuItem";
+            this.选择文件模板ToolStripMenuItem.Size = new System.Drawing.Size(113, 24);
+            this.选择文件模板ToolStripMenuItem.Text = "选择文件模板";
+            this.选择文件模板ToolStripMenuItem.Click += new System.EventHandler(this.选择文件模板ToolStripMenuItem_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(490, 30);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(172, 15);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "当前选择的文件模板为：";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(686, 29);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 15);
+            this.label9.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "ExcelToWord";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 使用帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 导入文件ToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem 开始ToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem 选择保存路径ToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ToolStripMenuItem 选择文件模板ToolStripMenuItem;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using NPOI;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.XWPF.UserModel;
+using MSWord = Microsoft.Office.Interop.Word;
+using NPOI.OpenXmlFormats.Wordprocessing;
+using System.Reflection;
+
+namespace WindowsForms_Word_To_Excel
+{
+    public partial class Form1 : Form
+    {
+        private string input_filename, filename_end, outputfile_foilder, tmp_info_file;
+        object doc_standard, tmp_info_name;
+        private List<string> FirstClassName = new List<string>();
+        private List<string> output_filenames = new List<string>();
+        private string[] info_names = { "姓名", "一级单位", "到达退休年龄时间", "领退休金时间", "递交时间", "申报备案时间", "最后署名时间" };
+        private string[] bookmark_names = { "Name","FirstClass", "RetireTime1", "RetireTime2", "SubmitTime1", "SubmitTime2", "DeclareTime1", "DeclareTime2","LastSignTime" };
+        string[] info_tmp = new string[7];
+        string[] info_use = new string[9];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            input_filename = "";
+            filename_end = "退休告知";
+            outputfile_foilder=System.AppDomain.CurrentDomain.BaseDirectory;
+            label7.Text = outputfile_foilder;
+            tmp_info_file = "tmp.docx";
+            tmp_info_name = tmp_info_file;
+            doc_standard = "";
+        }
+
+        private void 导入文件ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /*选择并打开文件，读取数据，关闭文件*/
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+            dialog.Title = "请选择一个符合格式的excel表格";
+            dialog.Filter = "Excel文件(*.xlsx)|*.xlsx";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                input_filename = dialog.FileName;
+            }
+            label1.Text = input_filename;
+        }
+
+        private void 选择保存路径ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.Description = "请选择目标文件夹";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(dialog.SelectedPath))
+                {
+                    MessageBox.Show(this, "文件夹路径不能为空", "提示");
+                    return;
+                }
+            }
+            outputfile_foilder = dialog.SelectedPath;
+            label7.Text = outputfile_foilder;
+        }
+
+        private void 选择文件模板ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /*选择并打开文件，读取数据，关闭文件*/
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+            dialog.Title = "请选择一个符合格式的word文件";
+            dialog.Filter = "Excel文件(*.docx)|*.docx";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                doc_standard = dialog.FileName;
+            }
+            label9.Text = doc_standard.ToString();
+        }
+
+        /// <summary>
+        /// 从模板文件（一个word）
+        /// </summary>
+        /// <param name="sorceDocPath">源文件路径</param>
+        /// <returns></returns>
+        protected MSWord.Document copyWordDoc(object sorceDocPath)
+        {
+            MSWord.Application wordApp;//Word应用程序变量 
+            MSWord.Document newWordDoc;//Word文档变量
+            object readOnly = false;
+            object isVisible = false;
+            //初始化
+            //由于使用的是COM库，因此有许多变量需要用Missing.Value代替
+            wordApp = new MSWord.Application();
+            Object Nothing = System.Reflection.Missing.Value;
+            newWordDoc = wordApp.Documents.Add(ref Nothing, ref Nothing, ref Nothing, ref Nothing);
+            MSWord.Document openWord;
+            openWord = wordApp.Documents.Open(ref sorceDocPath, ref Nothing, ref readOnly, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref isVisible, ref Nothing, ref Nothing, ref Nothing, ref Nothing);
+            openWord.Select();
+            openWord.Sections[1].Range.Copy();
+            object start = 0;
+            MSWord.Range newRang = newWordDoc.Range(ref start, ref start);
+            newWordDoc.Sections[1].Range.PasteAndFormat(MSWord.WdRecoveryType.wdPasteDefault);
+            openWord.Close(ref Nothing, ref Nothing, ref Nothing);
+            return newWordDoc;
+        }
+        
+        /*处理从excel中获取的字符串*/
+        protected void use_str_info()
+        {
+            for (int i = 0; i < 4; i++)
+                info_use[i] = info_tmp[i];
+            info_use[4] = info_tmp[4].Substring(0, 10);//SubmitTime1
+            info_use[5] = info_tmp[4].Substring(12);//SubmitTime2
+            info_use[6] = info_tmp[5].Substring(0, 10);//DeclareTime1
+            info_use[7] = info_tmp[5].Substring(12);//DeclareTime2
+            info_use[8] = info_tmp[6];//LastSignTime
+        }
+        
+        protected void Delete_tmp()
+        {
+            if (File.Exists(tmp_info_file))
+                File.Delete(tmp_info_file);
+            return;
+        }
+
+        private void 开始ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /*变量区域*/
+            #region 变量
+            FileStream fs;
+            IWorkbook wk;
+            int i, j, line_num, column_num, table_column, table_row;//计数
+            bool file_ok = true;
+            string value_tmp, word_text;
+            object out_file_path, bkObj;
+            object readOnly = false;
+            object isVisible = false;
+            object start = 0;
+            Object Nothing = Missing.Value;
+            object unite = MSWord.WdUnits.wdStory;
+            MSWord.Application wordApp;                   //Word应用程序变量 
+            MSWord.Document wordDoc_tmp,openWord,newWordDoc;  //Word文档变量
+            XWPFDocument doc = new XWPFDocument();
+            #endregion
+
+            wordApp = new MSWord.Application(); //初始化
+            //由于使用的是COM库，因此有许多变量需要用Missing.Value代替
+            /*判断模板文件是否选择*/
+            if (doc_standard.ToString()=="")
+            {
+                MessageBox.Show("请先选择模板文件!");
+                return;
+            }
+            /*先判断是否已经打开文件*/
+            if (input_filename == "")
+            {
+                MessageBox.Show("请先选择文件!");
+                return;
+            }
+            /*判断是否符合格式，仅检查第一行*/
+            fs = File.OpenRead(input_filename);
+            if (fs == null)
+            {
+                MessageBox.Show("文件打开失败，请重新选择文件");
+                return;
+            }
+            wk = new XSSFWorkbook(fs);
+            fs.Close();
+            ISheet sheet = wk.GetSheetAt(0);
+            line_num = sheet.LastRowNum + 1;//总行数
+            IRow row = sheet.GetRow(0);  //读取当前行数据
+            column_num = row.LastCellNum;//总列数，实际为固定值
+            if (column_num != info_names.Length)
+            {
+                MessageBox.Show("文件格式不符合!请修改后重试");
+                return;
+            }
+            //LastRowNum 是当前表的总行数-1（注意）
+            for (i = 0; i < column_num; i++)
+            {
+                value_tmp = row.GetCell(i).ToString();
+                if (string.Compare(value_tmp, info_names[i]) != 0)
+                {
+                    file_ok = false;
+                    break;
+                }
+                //value_total += value_tmp + "\r\n";
+            }
+            if (!file_ok)
+            {
+                MessageBox.Show("第" + i + "列名称错误！请修改后尝试");
+                return;
+            }
+            /*判断文件名是否有问题*/
+            out_file_path = outputfile_foilder + "\\" +textBox2.Text.ToString()+".docx";//注意组合后的路径名称
+            if(File.Exists(out_file_path.ToString()))
+            {
+                MessageBox.Show("输入的文件已经存在，请重新输入！");
+                return;
+            }
+            //先定义一个外面的文件
+            /*循环遍历每一行，写入一个文件即可*/
+            newWordDoc = wordApp.Documents.Add(ref Nothing, ref Nothing, ref Nothing, ref Nothing);//初始化输出文件
+            /*循环体*/
+            for (i=1;i< line_num ;i++)  
+            {
+                label2.Text = "正在处理第"+i+"条信息……";
+                /*变量区*/
+                //string name_now, workplace_now, retire_year, retire_money_time, submit_time, declare_time, last_sign_time;
+                row = sheet.GetRow(i);
+                for(j=0;j<column_num;j++)
+                {
+                    info_tmp[j] = row.GetCell(j).ToString();
+                    textBox1.Text += "\r\n" + info_tmp[j];
+                }
+                use_str_info();//处理字符串到需要的格式
+                /*先复制到tmp文件中，再修改tmp中内容并复制，粘贴到新的文件中*/
+                #region 复制模板中内容到临时文件
+                wordDoc_tmp = wordApp.Documents.Add(ref Nothing, ref Nothing, ref Nothing, ref Nothing);//初始化中间文件
+                openWord = wordApp.Documents.Open(ref doc_standard, ref Nothing, ref readOnly, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref isVisible, ref Nothing, ref Nothing, ref Nothing, ref Nothing);
+                openWord.Select();//选中openword
+                openWord.Sections[1].Range.Copy();
+                MSWord.Range newRang = wordDoc_tmp.Range(ref start, ref start);//改为当前操作的文档
+                wordDoc_tmp.Sections[1].Range.PasteAndFormat(MSWord.WdRecoveryType.wdPasteDefault);
+                //导出到tmp文件中
+                wordDoc_tmp.SaveAs(ref tmp_info_name, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing);
+                openWord.Close();
+                #endregion
+                #region 修改文件内容，并复制到输出文件
+                //打开临时文件，根据内容进行修改
+                wordDoc_tmp.Select();
+                for (j = 0; j < 9; j++)//遍历书签进行插入
+                {
+                    bkObj = bookmark_names[j];//书签的字符串转成对应的object型
+                    wordDoc_tmp.Bookmarks.get_Item(ref bkObj).Select();//选中书签对应的位置
+                    wordApp.Selection.TypeText(info_use[j]);
+                }
+                wordDoc_tmp.Sections[1].Range.Copy();//复制临时文件内容
+                newWordDoc.Select();//选中文件
+                wordApp.Selection.EndKey(ref unite, ref Nothing);//将光标移到文本末尾
+                newWordDoc.Paragraphs.Last.Range.PasteAndFormat(MSWord.WdRecoveryType.wdPasteDefault);//在文件末尾插入复制后的文档
+                wordDoc_tmp.Close();//关闭文件
+                #endregion
+                Delete_tmp();//删除临时文件
+            }
+            //循环结束后保存以及关闭文件
+            //WdSaveFormat为Word 默认文档的保存格式
+            object format = MSWord.WdSaveFormat.wdFormatDocumentDefault;// office 2007就是wdFormatDocumentDefault
+            newWordDoc.SaveAs(ref out_file_path, ref format, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing);
+            newWordDoc.Close();
+            label2.Text = "处理完成";
+        }
+    }
+}
